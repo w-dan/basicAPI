75e4bff69c667a3c69344202e2033e55a5c5c0cc
w-dan <windowsdani@hotmail.com>
2020-05-05 16:45:44 +0200

    CORS handling and body parser

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');   // log
+const bodyParser = require('body-parser');
 
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {       // response adjusting, add headers
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Acces-Control-Allow-Headers', '*');
+
+    if(req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
