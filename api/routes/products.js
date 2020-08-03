ff90ac7b97beb96866756d258ae0e04e227047f3
w-dan <windowsdani@hotmail.com>
2020-08-03 23:39:21 +0200

    Validation and minor fixes

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,13 +3,29 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Product = require('../models/product');
+const product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+    .select('name price _id')
     .exec()
-    .then(docs =>{
-        console.log(docs);
-        res.status(200).json(docs);
+    .then(docs => {
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products' + doc._id
+                    }
+                }
+            })
+        };
+
+        res.status(200).json(response);
     })
     .catch(err => {
         console.log(err);
@@ -19,9 +35,10 @@ router.get('/', (req, res, next) => {
     });
 });
 
+// POST
 router.post('/', (req, res, next) => {
     const product = new Product({
-        _id: new mongoose.Types.ObjectId,
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
     });
@@ -29,9 +46,18 @@ router.post('/', (req, res, next) => {
     product
     .save()
     .then(result => {
+        console.log(result);
         res.status(201).json ({
             message: 'Handling POST requests to /products',
-            createdProduct: result
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products' + result._id
+                }
+            }
         });
     }) // stores in database
     .catch(err => { 
@@ -46,11 +72,19 @@ router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
 
     Product.findById(id)
+    .select('name price _id')
     .exec()
     .then(doc => {
         console.log("From database", doc);
         if (doc) {
-            res.status(200).json(doc);
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: 'GET',
+                    description: 'GET_ALL_PRODUCTS',
+                    url: 'http://localhost:3000/products'
+                }
+            });
         } else {
             res.status(404).json({ message: 'Not found' });
         }
@@ -73,12 +107,17 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-        console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product updated',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/products/' + id
+            }
+        });
     })
     .catch(err => {
         console.log(err);
-        res.status(200).json({ error: err });
+        res.status(500).json({ error: err });
     });
 });
 
@@ -90,8 +129,16 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
     .exec()
     .then(result => {
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/products/',
+                body: { name: 'String', price: 'Number' }
+            },
+        });
     })
+
     .catch(err => {
         console.log(err);
         res.status(500).json({
